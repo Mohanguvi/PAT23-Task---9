14e4dbf4bd6562bdda3f968a88cdd3eda66c0161
Mohan raj <147843991+Mohanguvi@users.noreply.github.com>
2023-12-06 02:21:15 +0530

    Expected output

---
+++ Expectedoutput.py
@@ -0,0 +1,8 @@
+# Given Python Code
+data = [10, 501, 22, 37, 100, 999, 87, 351]
+
+result = filter (lambda x: x > 4, data) # 
+
+print(list(result))
+
+# Expected output - [10, 501, 22, 37, 100, 999, 87, 351]
